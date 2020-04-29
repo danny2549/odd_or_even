de9746e1dfe983a459dab49c3e81408523b3076d
danny2549 <31489624+danny2549@users.noreply.github.com>
2020-04-29 08:41:42 -0400

    Add files via upload

---
+++ odd_or_even.py
@@ -0,0 +1,12 @@
+twenty = 20
+number = 0
+while twenty > number:
+    number += 1
+    if number == 4:
+        print(f'{number} is unlucky')
+    elif number == 13:
+        print(f'{number} is unlucky')
+    elif number % 2:
+        print(f"{number} is even")
+    elif:
+        print(f"{number} is odd")
